33e10329610ff4e7e96fae1b4998d0f7a604d0ef
Errol Lee <62831245+errolleesw@users.noreply.github.com>
2023-03-18 13:12:07 +1100

    update exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...argToRemove) {
+    return arr.filter(item => !argToRemove.includes(item))
 };
 
+console.log(removeFromArray(['one','two','three'],'one','two'));
 // Do not edit below this line
 module.exports = removeFromArray;
