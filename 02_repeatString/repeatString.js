41651a5e08320d4cb0b6fb83d4fce18b1a8f3a54
Errol Lee <62831245+errolleesw@users.noreply.github.com>
2023-03-18 13:02:03 +1100

    solution for exercise 03

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let repeatedStr = '';
+    if (num < 0) {
+        return 'ERROR';
+    } else {
+        for (let i=0; i < num; i++) {
+            repeatedStr += str;
+        }
+        return repeatedStr;
+    }
+    
 };
 
 // Do not edit below this line
