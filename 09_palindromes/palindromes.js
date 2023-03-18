b3eb9a10172862657da8b2d4581a69bb7da16d14
Errol Lee <62831245+errolleesw@users.noreply.github.com>
2023-03-18 14:16:29 +1100

    update exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    // convert string to lowercase and remove all non-alphanumeric characters
+    str = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+    
+    // compare the reversed string with the original string
+    return str === str.split('').reverse().join('');
 };
 
+
+console.log(palindromes('racecar'));
 // Do not edit below this line
 module.exports = palindromes;
