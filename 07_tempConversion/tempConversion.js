d9884e65607f6b0dceeb54f20163a9cd8237e9c9
Errol Lee <62831245+errolleesw@users.noreply.github.com>
2023-03-18 13:34:12 +1100

    Update Exercies 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  const celsius = (fahrenheit - 32) / 1.8;
+  return celsius % 1 === 0 ? parseInt(celsius, 10) : parseFloat(celsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  const fahrenheit = celsius * 1.8 + 32;
+  return fahrenheit % 1 === 0 ? parseInt(fahrenheit, 10) : parseFloat(fahrenheit.toFixed(1));
 };
+
+console.log(convertToCelsius(32));
+console.log(convertToFahrenheit(0));
 
 // Do not edit below this line
 module.exports = {
