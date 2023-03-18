28423380bcd7a82c027732fdc6c734772bc606e5
Errol Lee <62831245+errolleesw@users.noreply.github.com>
2023-03-18 14:24:14 +1100

    Update exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,24 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+    if (n < 0) {
+        return 'OOPS';
+    }
+    if (n === 1 || n === 2) {
+        return 1;
+    }
+    let a = 1;
+    let b = 1;
+    for (let i = 3; i <= n; i++) {
+        let c = a + b;
+        // console.log(a);
+        // console.log(b);
+        // console.log(c);
+        a = b;
+        b = c;
+    }
+    return b;
 };
+
+console.log(fibonacci(4));
 
 // Do not edit below this line
 module.exports = fibonacci;
